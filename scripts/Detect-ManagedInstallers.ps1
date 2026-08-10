0aec1e4b1c322e955fe21f9f6db3d4c94a559e20
Rory Vossepoel <roryvossepoel@gmail.com>
2026-08-10 15:38:20 +0200

    Set detection version to 1.0.0

--- scripts/Detect-ManagedInstallers.ps1
+++ scripts/Detect-ManagedInstallers.ps1
@@ -1,9 +1,9 @@
 #requires -version 5.1
 
-# Toolkit version: 0.1.4
+# Toolkit version: 1.0.0
 
 $ErrorActionPreference = 'Stop'
-$toolkitVersion = '0.1.4'
+$toolkitVersion = '1.0.0'
 Write-Output "App Control for Business Managed Installer Toolkit version $toolkitVersion"
 $policyRoot = 'HKLM:\Software\Policies\ManagedInstallers'
 $managedMarkers = @('ManagedInstallers:')
